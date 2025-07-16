da3b8f6206599de01ad5b1598a1a7a395bdb9634
VacaSama <klc_moore@outlook.com>
2025-07-16 15:43:07 -0700

    Updated Navigation and Added Animal managment features, currently the Animal class has properties : Classfication, Species and ExibitOpen.
    Create() view was created, and sends ViewData to the Title in the Layout.cshtml.
    Index View has not been implemented

---
+++ ModelsAndModelBinding/Controllers/AnimalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelsAndModelBinding.Controllers;
+
+public class AnimalController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
---
+++ ModelsAndModelBinding/Models/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace ModelsAndModelBinding.Models;
+
+/// <summary>
+/// Animals in the zoo, tells the user which exibits are open to visiting and which are closed
+/// </summary>
+public class Animal
+{
+    // what properties are needed for an animal?
+    public string Classification { get; set; } 
+    public string Species { get; set; }
+    // idea may have to be chanaged to a store that sells animal plushies. 
+    // public bool InStock {get; set; } = true; // default value
+    // public int price {get; set;}
+    public bool ExibitOpen { get; private set; } = true; // default value
+
+    public Animal(string classification, string species, bool exibitOpen)
+    {
+        Classification = classification;
+        Species = species;
+        ExibitOpen = exibitOpen;
+    }
+}
